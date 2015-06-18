2e370136444303f7f40daeaa85a2ed884b1f75e9
TommyCakes <tomhwarhurst@gmail.com>
2015-06-18 13:18:16 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,34 +1,53 @@
 $(document).ready(function() {
 	//Trash icon now deletes text bar.//
-	$(".trash1").click(function() {
-		$('.t1, .trash1, .ticks1').remove();
+$('.ticks').on('click' ,function(){
+		$('p').next(this).css("text-decoration", "line-through");
+		
 	});
-	$(".trash2").click(function() {
-		$('.t2, .trash2, .ticks2').remove();
+
+$(".trash").on('click', function() {
+	$(this).next('.t,.t1').remove();
+$(this).next('.ticks').remove();
+$(this).remove()
+
 	});
-	$(".trash3").click(function() {
-		$('.t3, .trash3, .ticks3').remove();
-	});
-	$(".trash4").click(function() {
-		$('.t4, .trash4, .ticks4').remove();
-	});
-	//Text is deleted when you click on the bar//
-	$(".t1").click(function() {
-		$('.t1 > p ').hide();
-		$('.i1').show();
-	});
-	$(".t2").click(function() {
-		$('.t2 > p').hide();
-		$('i2').show();
-	});
-	$(".t3").click(function() {
-		$('.t3 > p').hide();
-		$('i3').show();
-	});
-	$(".t4").click(function() {
-		$('.t4 > p').hide();
-		$('i4').show();
-	});	
+	$('body').on('keydown', function(e){
+		if(e.which == 13) {
+		$('.empty').show();	
+		$(this).after("<div class='empty'>" +  "</div>");
+	}
+	else {
+		return
+	}
+
+});
+	$(".t").click(function() {
+		$('p').hide();
+		$('.hidden').show();
+
+});
 });
 
+
+	
+
+
+//$(".ticks").on('click',function() {
+		//console.log(this)
+		//console.log($(this).previous('.t'))
+	//});
+	
+	
+	//$(".t").click(function() {
+		//$('.t > p ').hide();
+		//$('.t').show();
+		//$('.t').removeClass('hidden').focus();
+
+
+		//Text is deleted when you click on the bar//
+		//.on('click', class with .completed)
+
 //display:none
+//,.trash2 ,.trash3 ,.trash4 
+
+//.prev('.hidden')
